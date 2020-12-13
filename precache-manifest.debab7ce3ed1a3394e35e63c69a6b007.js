fd1e32a2ee6d70782f6528fe1fb221e0c65f920b
Amir Suhail <amir.suhail@arista.com>
2020-12-13 20:10:27 +0530

    Updates

---
+++ precache-manifest.debab7ce3ed1a3394e35e63c69a6b007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07988387bbc2f3de1a8b",
+    "url": "/trello-react/static/css/main.919af0eb.chunk.css"
+  },
+  {
+    "revision": "07988387bbc2f3de1a8b",
+    "url": "/trello-react/static/js/main.07988387.chunk.js"
+  },
+  {
+    "revision": "4a83e13af1964ccd7c9a",
+    "url": "/trello-react/static/js/runtime~main.4a83e13a.js"
+  },
+  {
+    "revision": "0e44ed756545ac08f851",
+    "url": "/trello-react/static/js/2.0e44ed75.chunk.js"
+  },
+  {
+    "revision": "0b1ab1d4490d42d1d5362cbad36cd22b",
+    "url": "/trello-react/index.html"
+  }
+];
